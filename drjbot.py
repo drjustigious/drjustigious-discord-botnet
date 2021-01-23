d8f0daf0aa4d2ff72ff5e23f7d4a38cb1ef715bd
Jussi Tikkanen <drjustigious@gmail.com>
2021-01-23 19:15:40 +0200

    Attach a cog to the bot

--- drjbot.py
+++ drjbot.py
@@ -1,22 +1,29 @@
 import os
-import discord
+from discord.ext import commands
 import dotenv
+import random
+
+from Cogs import ExperimentalCogs
 
 dotenv.load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_DEFAULT_GUILD')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+bot.add_cog(ExperimentalCogs.Greetings(bot))
 
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+@bot.command(name='99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
 
-client.run(TOKEN)
+bot.run(TOKEN)
