b3e847bb868719ce2360c903a9328f50033ed6ec
Jussi Tikkanen <drjustigious@gmail.com>
2021-01-29 17:26:31 +0200

    Weather reports implemented

--- Cogs/Weather.py
+++ Cogs/Weather.py
@@ -1,6 +1,7 @@
 import os
 import json
 import logging
+import requests
 
 import fuzzywuzzy.process
 from discord.ext import commands
@@ -85,9 +86,9 @@ class Weather(commands.Cog):
             return
 
         # Seems like we have a reasonable guess for the town the user asked about.
-        town_resolution_comment = f"<@{ctx.author.id}>, here's the weather forecast for {target_town['name']}."
+        town_resolution_comment = f"<@{ctx.author.id}> Here's the weather forecast for {target_town['name']}."
         if matching_percentage < 100:
-            town_resolution_comment = f"<@{ctx.author.id}>, I'm {matching_percentage}% sure you meant a town called {target_town['name']}, here's their weather forecast."
+            town_resolution_comment = f"<@{ctx.author.id}> I'm {matching_percentage}% sure you meant a town called {target_town['name']}, here's their weather forecast."
 
         weather_forecast = await self.fetch_weather_forecast(target_town)
         message = f"{town_resolution_comment}\n```\n{weather_forecast}```"
@@ -96,4 +97,97 @@ class Weather(commands.Cog):
 
 
     async def fetch_weather_forecast(self, target_town):
-        return "Some\nlines\nof randomness.\n"
+
+        api_key = os.getenv("OPENWEATHER_API_KEY")
+
+        url = f"https://api.openweathermap.org/data/2.5/forecast?units=metric&id={target_town['id']}&appid={api_key}"
+        response = requests.get(url=url)
+        if not response.ok:
+            logging.error(f"Error getting weather data: {response.status_code} {response.reason}\n{response.text}")
+            return f"Error getting weather data: {response.status_code} {response.reason}. Somebody should check the bot's logs for details."
+
+        report_lines = self.process_weather_report(
+            response.json()
+        )
+
+        report_message = "\n".join(report_lines)
+        return report_message
+
+
+    def process_weather_report(self, data):
+        target_timestamps = [
+            "00:00:00",
+            "06:00:00",
+            "12:00:00",
+            "18:00:00",
+        ]
+
+        num_reports = 4
+
+        target_entries = [
+            entry for entry in data.get("list", []) if any (
+                timestamp in entry.get("dt_txt") for timestamp in target_timestamps
+            )
+        ][:num_reports]
+
+        report_lines = []
+        for entry in target_entries:
+            report_lines.append(
+                self.render_weather_report_datapoint(entry)
+            )
+
+        return report_lines
+
+
+    def render_weather_report_datapoint(self, entry):
+        line = f"[{entry.get('dt_txt', 'unknown time')}]\n"
+
+        main = entry.get("main", {})
+        temperature = f"{main.get('temp', '??')} °C"
+        pressure = f"{main.get('pressure', '??')} hPa"
+        humidity = f"{main.get('humidity', '??')}%"
+        line += f"  T = {temperature}, P = {pressure}, RH = {humidity}\n"
+
+        wind = entry.get("wind", {})
+        degrees = wind.get('deg', 0)
+        line += f"  wind: {wind.get('speed', '??')} m/s from {degrees}° ({self.describe_compass_angle(degrees)})\n"
+
+        clouds = entry.get("clouds", {})
+        line += f"  {clouds.get('all', '??')}% cloudy\n"
+
+        got_precipitation = False
+
+        if "snow" in entry:
+            snow = entry["snow"]
+            lst = list(snow.values())
+            amount = lst[0] if lst else 0.00
+            line += f"  snow {amount} mm in 3 h\n"
+            got_precipitation = True
+
+        if "rain" in entry:
+            rain = entry["rain"]
+            lst = list(rain.values())
+            amount = lst[0] if lst else 0.00
+            line += f"  rain {amount} mm in 3 h\n"
+            got_precipitation = True
+
+        if not got_precipitation:
+            line += f"  no precipitation\n"
+
+        weather_descriptions = entry.get("weather", [])
+        description_strings = []
+        for description in weather_descriptions:
+            description_strings.append(
+                description.get('description', 'undescribed weather')
+            )
+
+        line += "  "+", ".join(description_strings)
+
+        return line
+
+
+    def describe_compass_angle(self, degrees):
+        index = int(degrees/22.5 + 0.5)
+        choices = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
+
+        return choices[(index % 16)]
