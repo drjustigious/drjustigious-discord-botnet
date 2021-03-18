a2e4093eec2a8aa2ad94e7f47d0e6267001745a7
Jussi Tikkanen <44530293+drjustigious@users.noreply.github.com>
2021-03-18 21:43:42 +0200

    Echo a message when starting to install the venv
    There can be a considerable time gap between when apt finishes and pip starts to output something related to the virtual environment setup.

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,7 @@ sudo apt install -y python3 python3-venv
 
 
 # Create virtual Python environment and install dependencies.
+echo -e "\nConfiguring virtual Python environment.\n"
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
